3ac3d28bbd12abe9f68c87973b063f7f236a2d53
Isabel <Isabel@Isa>
2015-11-08 13:37:59 +0100

    v1

--- src/iescomercio/tema4/cafetera/Run.java
+++ src/iescomercio/tema4/cafetera/Run.java
@@ -10,5 +10,14 @@ package iescomercio.tema4.cafetera;
  * @author VESPERTINO
  */
 public class Run {
-    
+
+    public static void main(String[] args) {
+        Cafetera miCafetera;
+        miCafetera = new Cafetera(500, 200);
+        
+        System.out.println(miCafetera.getCantidadActual());
+        miCafetera.servirTaza(250);
+        System.out.println(miCafetera.getCantidadActual());
+        
+    }
 }

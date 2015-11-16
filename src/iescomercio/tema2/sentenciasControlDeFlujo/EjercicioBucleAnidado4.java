717e53cd8b3446f40bc185bf7d56a2d45eeaf5ff
VESPERTINO <VESPERTINO@C8PC14>
2015-11-16 17:05:48 +0100

    bucles

---
+++ src/iescomercio/tema2/sentenciasControlDeFlujo/EjercicioBucleAnidado4.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package iescomercio.tema2.sentenciasControlDeFlujo;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author VESPERTINO
+ */
+public class EjercicioBucleAnidado4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+        System.out.println("Introduce un numero");
+        n = sc.nextInt();
+        
+        for (int i=1; 1 <=n; i++){
+            for(int j= 1; j<= n; j++){
+                System.out.println("*");
+            }
+        }
+    }
+}
